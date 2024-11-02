3d0b0409d9bc9f2aacdcacb3f4fc95460ba67608
Salty <jirayutjujaiboon123@gmail.com>
2024-11-03 03:00:00 +0700

    import commit

---
+++ app/(tabs)/settings/index.tsx
@@ -0,0 +1,14 @@
+import GradientBackground from "@/components/GredientBackground";
+import { View, Text } from "react-native";
+
+const LoginPage = () => {
+  return (
+    <GradientBackground>
+      <View>
+        <Text>Settings Page</Text>
+      </View>
+    </GradientBackground>
+  );
+};
+
+export default LoginPage;
