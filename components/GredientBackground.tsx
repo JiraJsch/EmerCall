68fc4ee0efc6f2ed24c0fd93594fa0cda5a413e7
Salty <66292786@siamtech.ac.th>
2024-12-14 12:50:16 +0700

    clean up code

--- components/GredientBackground.tsx
+++ components/GredientBackground.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react";
 import { LinearGradient } from "expo-linear-gradient";
-import { StyleSheet, ViewStyle } from "react-native";
+import { ViewStyle } from "react-native";
 
 interface GradientBackgroundProps {
   children: ReactNode;
@@ -14,17 +14,11 @@ const GradientBackground: React.FC<GradientBackgroundProps> = ({
   return (
     <LinearGradient
       colors={["#394db3", "#db2265"]}
-      style={[styles.gradient, style, { flex: 1 }]}
+      style={[style, { flex: 1 }]}
     >
       {children}
     </LinearGradient>
   );
 };
-
-const styles = StyleSheet.create({
-  gradient: {
-    flex: 1,
-  },
-});
 
 export default GradientBackground;
